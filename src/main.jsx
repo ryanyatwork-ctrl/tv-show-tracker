f5e1371b0eacdebc6c98d05441b35bc5e288cd3e
Mind4Metal <ryanyatwork@gmail.com>
2025-11-09 15:51:20 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import TVShowTracker from "./TVShowTracker.jsx";
+
+console.log("TVTracker booting…");
 createRoot(document.getElementById("root")).render(<TVShowTracker />);
