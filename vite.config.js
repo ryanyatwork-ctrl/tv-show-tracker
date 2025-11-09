6fc05174e6646b3dc976f5109f03acf86057abbe
Mind4Metal <ryanyatwork@gmail.com>
2025-11-09 15:19:11 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: "/",
-  build: { outDir: "docs" } // Pages will serve from /docs on main
-});
+  plugins: [react()],
+})
